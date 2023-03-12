c223efd3bbc9061389bc4ed5bbf3690585894161
Umut Palabiyik <57181469+UmutPalabiyik@users.noreply.github.com>
2023-03-12 06:49:46 +0300

    edit categories's button style

--- src/components/Header/CategoriesPopoverContent.tsx
+++ src/components/Header/CategoriesPopoverContent.tsx
@@ -8,9 +8,9 @@ const CategoriesPopoverContent = () => {
 
     return (
         <Box className="flex flex-col w-[156px] p-3 bg-[#00254F] rounded  gap-y-1 ">
-            <p className="text-sm text-white font-['Roboto'] leading-[26px]">Data</p>
+            <p className="text-sm text-white font-['Roboto'] leading-[26px] py-1 px-2 rounded-sm">Data</p>
             {
-                categories.map((category: string, key) => <a className="text-sm text-white font-['Roboto'] leading-[26px]" key={key}>{category}</a>)
+                categories.map((category: string, key) => <a className="text-sm text-white font-['Roboto'] py-1 px-2 rounded-sm leading-[26px] hover:bg-[#0059BC]" key={key}>{category}</a>)
             }
         </Box>
     )
